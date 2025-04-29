dc1163db303cd7a97645bc68c338d3757379212d
breinersn <bsuarezn1@unicartagena.edu.co>
2025-04-29 13:57:28 -0500

    primer commit

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
 # flutter_signal
 
-A new Flutter project.
+proyecto de flutter que implementa signal como manejador de estado
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+- signals_flutter: ^6.0.2
--- lib/signals/config.signal.dart
+++ lib/signals/config.signal.dart
@@ -1,30 +1,30 @@
-// import 'package:signals_flutter/signals_flutter.dart';
+import 'package:signals_flutter/signals_flutter.dart';
 
-// class Config {
-//   String name;
-//   int counter;
-//   bool darkMode;
+class Config {
+  String name;
+  int counter;
+  bool darkMode;
 
-//   Config({
-//     required this.name,
-//     required this.counter,
-//     required this.darkMode,
-//   });
+  Config({
+    required this.name,
+    required this.counter,
+    required this.darkMode,
+  });
 
-//   Config copyWith({
-//     String? name,
-//     int? counter,
-//     bool? darkMode,
-//   }) {
-//     return Config(
-//         name: name ?? this.name,
-//         counter: counter ?? this.counter,
-//         darkMode: darkMode ?? this.darkMode);
-//   }
-// }
+  Config copyWith({
+    String? name,
+    int? counter,
+    bool? darkMode,
+  }) {
+    return Config(
+        name: name ?? this.name,
+        counter: counter ?? this.counter,
+        darkMode: darkMode ?? this.darkMode);
+  }
+}
 
-// final configSignal = signal<Config>(Config(
-//   name: "breiner sn",
-//   counter: 1,
-//   darkMode: false,
-// ));
+final configSignal = signal<Config>(Config(
+  name: "breiner sn",
+  counter: 1,
+  darkMode: false,
+));
